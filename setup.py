d1de166648af8f24135426c2bc230e59a8f11b4c
vivien <826197021@qq.com>
2022-08-09 17:12:46 +0800

    release new version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ data_files = [
 
 setuptools.setup(
     name='amiyabot',
-    version='1.1.0',
+    version='1.1.1',
     author='vivien8261',
     author_email='826197021@qq.com',
     url='https://www.amiyabot.com',
