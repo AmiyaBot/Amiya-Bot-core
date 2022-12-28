57e8588676f50f1ecec3614d5bde19337289c25b
Vivien <34387011+vivien8261@users.noreply.github.com>
2022-12-28 11:43:08 +0800

    update 定时任务支持子级标签

--- amiyabot/builtin/lib/timedTask.py
+++ amiyabot/builtin/lib/timedTask.py
@@ -28,7 +28,7 @@ class TasksControl(metaclass=Singleton):
         self.alive = True
         self.start = False
 
-        self._timed_tasks: Dict[str, List[TimedTask]] = dict()
+        self._timed_tasks: Dict[str, Dict[str, List[TimedTask]]] = dict()
 
         ServerEventHandler.on_shutdown.append(self.stop)
 
@@ -52,36 +52,55 @@ class TasksControl(metaclass=Singleton):
                 if not self._timed_tasks:
                     continue
 
-                for tag, tasks in self._timed_tasks.items():
-                    for task in tasks:
-                        if await task.check(t):
-                            async with log.catch('TimedTask Error:'):
-                                await task.task()
+                ready = []
+                for tag, group in self._timed_tasks.items():
+                    for sub_tag, tasks in group.items():
+                        for task in tasks:
+                            if await task.check(t):
+                                ready.append(task)
+
+                for item in ready:
+                    async with log.catch('TimedTask Error:'):
+                        await item.task()
 
         except KeyboardInterrupt:
             pass
 
-    def timed_task(self, each: int = None, custom: CUSTOM_CHECK = None, tag: str = 'no_tag'):
+    def timed_task(self,
+                   each: int = None,
+                   custom: CUSTOM_CHECK = None,
+                   tag: str = '__default__',
+                   sub_tag: str = '__sub__'):
         """
         注册定时任务
         非严格定时，因为执行协程会产生切换的耗时。所以此注册器定义的循环时间为"约等于"。
 
-        :param each:     循环执行间隔时间，单位（秒）
-        :param custom:   自定义循环规则
-        :param tag:      标签
+        :param each:    循环执行间隔时间，单位（秒）
+        :param custom:  自定义循环规则
+        :param tag:     标签
+        :param sub_tag: 子标签
         :return:
         """
 
         def register(task: TASK_CORO):
             if tag not in self._timed_tasks:
-                self._timed_tasks[tag] = []
+                self._timed_tasks[tag] = {}
 
-            self._timed_tasks[tag].append(TimedTask(task, each, custom))
+            if sub_tag not in self._timed_tasks[tag]:
+                self._timed_tasks[tag][sub_tag] = []
+
+            self._timed_tasks[tag][sub_tag].append(TimedTask(task, each, custom))
 
         return register
 
-    def remove_tag(self, tag: str):
-        if tag in self._timed_tasks:
+    def remove_tag(self, tag: str, sub_tag: str = None):
+        if tag not in self._timed_tasks:
+            return None
+
+        if sub_tag:
+            if sub_tag in self._timed_tasks[tag]:
+                del self._timed_tasks[tag][sub_tag]
+        else:
             del self._timed_tasks[tag]
 
 
