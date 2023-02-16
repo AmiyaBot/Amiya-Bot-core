d7b1fdd356aaef9a41cec0e9ba67a98da58de322
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-02-16 15:19:35 +0800

    update QQ-Bot 适配器新增消息发送失败重试

--- amiyabot/network/httpRequests.py
+++ amiyabot/network/httpRequests.py
@@ -3,7 +3,6 @@ import aiohttp
 
 from typing import Union
 from amiyabot import log
-from contextlib import asynccontextmanager
 
 
 class HttpRequests:
@@ -11,30 +10,39 @@ class HttpRequests:
     async_success = [201, 202, 304023, 304024]
 
     @classmethod
-    @asynccontextmanager
-    async def __handle_requests(cls, method: str, interface: str):
-        async def handler(res: aiohttp.ClientResponse):
-            if res.status in cls.success:
-                return await res.text()
-            elif res.status in cls.async_success:
-                pass
-            else:
-                log.error(f'bad to request <{interface}>[{method}]. Got code {res.status}')
-
+    async def request(cls,
+                      url: str,
+                      method: str = 'post',
+                      request_name: str = None,
+                      **kwargs):
         try:
+            request_name = (request_name or method).upper()
+
             async with aiohttp.ClientSession() as session:
-                yield session, handler
+                async with session.request(method, url, **kwargs) as res:
+                    if res.status in cls.success:
+                        return await res.text()
+                    elif res.status in cls.async_success:
+                        return ''
+                    else:
+                        log.error(f'bad to request <{url}>[{request_name}]. Got code {res.status}')
         except aiohttp.ClientConnectorError:
-            log.error(f'fail to request <{interface}>[{method}]')
+            log.error(f'fail to request <{url}>[{request_name}]')
         except Exception as e:
             log.error(e)
 
     @classmethod
-    async def get(cls, interface: str, **kwargs):
+    async def get(cls,
+                  interface: str,
+                  **kwargs):
         return await cls.request(interface, 'get', **kwargs)
 
     @classmethod
-    async def post(cls, interface: str, payload: Union[dict, list] = None, headers: dict = None):
+    async def post(cls,
+                   interface: str,
+                   payload: Union[dict, list] = None,
+                   headers: dict = None,
+                   **kwargs):
         _headers = {
             'Content-Type': 'application/json',
             **(headers or {})
@@ -42,16 +50,20 @@ class HttpRequests:
         _payload = {
             **(payload or {})
         }
-        return await cls.request(interface, 'post', data=json.dumps(_payload), headers=_headers)
+        return await cls.request(interface, 'post', data=json.dumps(_payload), headers=_headers, **kwargs)
 
     @classmethod
-    async def request(cls, url: str, method: str = 'post', **kwargs):
-        async with cls.__handle_requests(method.upper(), url) as (session, handler):
-            async with session.request(method, url, **kwargs) as res:
-                return await handler(res)
+    async def post_upload(cls,
+                          interface: str,
+                          file: bytes,
+                          file_field: str = 'file',
+                          payload: dict = None,
+                          headers: dict = None,
+                          **kwargs):
+        _headers = {
+            **(headers or {})
+        }
 
-    @classmethod
-    async def upload(cls, interface: str, file: bytes, file_field: str = 'file', payload: dict = None):
         data = aiohttp.FormData()
         data.add_field(file_field,
                        file,
@@ -60,12 +72,14 @@ class HttpRequests:
         for field, value in (payload or {}).items():
             data.add_field(field, value)
 
-        async with cls.__handle_requests('UPLOAD', interface) as (session, handler):
-            async with session.post(interface, data=data) as res:
-                return await handler(res)
+        return await cls.request(interface, 'post', 'post-upload', data=data, **kwargs)
 
     @classmethod
-    async def post_form_data(cls, interface: str, payload: dict = None, headers: dict = None):
+    async def post_form(cls,
+                        interface: str,
+                        payload: dict = None,
+                        headers: dict = None,
+                        **kwargs):
         _headers = {
             **(headers or {})
         }
@@ -78,9 +92,7 @@ class HttpRequests:
                 value = json.dumps(value, ensure_ascii=False)
             data.add_field(field, value)
 
-        async with cls.__handle_requests('FORM POST', interface) as (session, handler):
-            async with session.post(interface, data=data, headers=_headers) as res:
-                return await handler(res)
+        return await cls.request(interface, 'post', 'post-form', data=data, headers=_headers, **kwargs)
 
 
 http_requests = HttpRequests
