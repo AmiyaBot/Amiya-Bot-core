7fac8b57d38487bb58ec0d92064cef425fe6a306
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-03-02 19:08:58 +0800

    update 支持自动创建数据库

--- amiyabot/network/download.py
+++ amiyabot/network/download.py
@@ -12,7 +12,7 @@ default_headers = {
 
 def download_sync(url: str, headers=None, stringify=False, progress=False):
     try:
-        stream = requests.get(url, headers=headers or default_headers, stream=True)
+        stream = requests.get(url, headers={**default_headers, **(headers or {})}, stream=True)
         container = BytesIO()
 
         if stream.status_code == 200:
@@ -41,7 +41,7 @@ def download_sync(url: str, headers=None, stringify=False, progress=False):
 async def download_async(url, headers=None, stringify=False):
     async with log.catch('download error:', ignore=[requests.exceptions.SSLError]):
         async with aiohttp.ClientSession() as session:
-            async with session.get(url, headers=headers or default_headers) as res:
+            async with session.get(url, headers={**default_headers, **(headers or {})}) as res:
                 if res.status == 200:
                     if stringify:
                         return await res.text()
