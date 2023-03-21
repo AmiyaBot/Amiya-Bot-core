f9b8776dbb9d961ee73b83c0a49dacd3a79113d0
Vivien <34387011+vivien8261@users.noreply.github.com>
2023-03-21 10:37:35 +0800

    update http server

--- amiyabot/network/httpServer/__init__.py
+++ amiyabot/network/httpServer/__init__.py
@@ -1,55 +1,17 @@
 import os
-import asyncio
-import uvicorn
 
-from typing import Any, List, Callable
+from typing import Any
 from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
 
-from pydantic import BaseModel
-
-from amiyabot.log import LoggerManager
 from amiyabot.util import snake_case_to_pascal_case
+
+from .serverBase import *
 
 cur_file_path = os.path.abspath(__file__)
 cur_file_folder = os.path.dirname(cur_file_path)
-
-
-class ServerLog:
-    logger = LoggerManager('Server')
-
-    @classmethod
-    def write(cls, text: str):
-        cls.logger.info(text, 'server')
-
-
-class ServerEventHandler:
-    on_shutdown: List[Callable] = []
-
-
-class ServerMeta(type):
-    servers: List[uvicorn.Server] = []
-    shutdown_lock = False
-
-    def __call__(cls, *args, **kwargs):
-        instance = super().__call__(*args, **kwargs)
-
-        cls.servers.append(instance.server)
-
-        return instance
-
-    def shutdown_all(cls, server: uvicorn.Server):
-        if not cls.shutdown_lock:
-            cls.shutdown_lock = True
-
-            for item in cls.servers:
-                if item != server:
-                    item.should_exit = True
-
-            for action in ServerEventHandler.on_shutdown:
-                asyncio.create_task(action())
 
 
 class HttpServer(metaclass=ServerMeta):
@@ -74,7 +36,7 @@ class HttpServer(metaclass=ServerMeta):
         self.__allow_path = []
 
         @self.app.middleware('http')
-        async def interceptor(request: Request, call_next):
+        async def interceptor(request: Request, call_next: Callable):
             if not request.scope['path'] in self.__allow_path + ['/docs', '/favicon.ico', '/openapi.json']:
                 if auth_key and request.headers.get('authKey') != auth_key:
                     return Response('Invalid authKey', status_code=401)
@@ -85,17 +47,16 @@ class HttpServer(metaclass=ServerMeta):
             HttpServer.shutdown_all(self.server)
 
     def set_allow_path(self, paths: list):
-        self.__allow_path = paths
+        self.__allow_path += paths
 
-    @staticmethod
-    def response(data: Any = None, code: int = 200, message: str = ''):
-        return {
-            'data': data,
-            'code': code,
-            'message': message
-        }
+    def __load_server(self, options):
+        return uvicorn.Server(config=uvicorn.Config(self.app,
+                                                    loop='asyncio',
+                                                    log_config=os.path.join(cur_file_folder,
+                                                                            '../../_assets/serverLogger.yaml'),
+                                                    **options))
 
-    def route(self, router_path: str = None, method: str = 'post', **kwargs):
+    def route(self, router_path: str = None, method: str = 'post', allow_unauthorized: bool = False, **kwargs):
         def decorator(fn):
             nonlocal router_path
 
@@ -117,17 +78,20 @@ class HttpServer(metaclass=ServerMeta):
             router = router_builder(**arguments)
 
             self.__routes.append(router_path)
+            if allow_unauthorized:
+                self.__allow_path.append(router_path)
 
             return router(fn)
 
         return decorator
 
-    def __load_server(self, options):
-        return uvicorn.Server(config=uvicorn.Config(self.app,
-                                                    loop='asyncio',
-                                                    log_config=os.path.join(cur_file_folder,
-                                                                            '../../_assets/serverLogger.yaml'),
-                                                    **options))
+    @staticmethod
+    def response(data: Any = None, code: int = 200, message: str = ''):
+        return {
+            'data': data,
+            'code': code,
+            'message': message
+        }
 
     async def serve(self):
         async with ServerLog.logger.catch('Http server Error:'):
---
+++ amiyabot/network/httpServer/serverBase.py
@@ -0,0 +1,41 @@
+import asyncio
+import uvicorn
+
+from typing import List, Callable
+from pydantic import BaseModel
+from amiyabot.log import LoggerManager
+
+
+class ServerLog:
+    logger = LoggerManager('Server')
+
+    @classmethod
+    def write(cls, text: str):
+        cls.logger.info(text, 'server')
+
+
+class ServerEventHandler:
+    on_shutdown: List[Callable] = []
+
+
+class ServerMeta(type):
+    servers: List[uvicorn.Server] = []
+    shutdown_lock = False
+
+    def __call__(cls, *args, **kwargs):
+        instance = super().__call__(*args, **kwargs)
+
+        cls.servers.append(instance.server)
+
+        return instance
+
+    def shutdown_all(cls, server: uvicorn.Server):
+        if not cls.shutdown_lock:
+            cls.shutdown_lock = True
+
+            for item in cls.servers:
+                if item != server:
+                    item.should_exit = True
+
+            for action in ServerEventHandler.on_shutdown:
+                asyncio.create_task(action())
