b7031fb6bd9ab8c52b2050f9ad516159b4bee7ff
hsyhhssyy <hsyhhssyy@gmail.com>
2023-11-15 03:19:21 +0000

    changed_setup

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 import os
 import json
 import setuptools
+from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
 
 from urllib import request
-
 
 def ver_num(_v):
     num = int(_v.replace('.', ''))
@@ -11,22 +11,62 @@ def ver_num(_v):
         num *= 10
     return num
 
+def get_new_version():
+    pypi = json.loads(request.urlopen('https://pypi.python.org/pypi/amiyabot/json').read())
+    v_list = {ver_num(v): v for v in pypi['releases'].keys()}
+    s_list = sorted(v_list)
+    latest = v_list[s_list[-1]]
 
-pypi = json.loads(request.urlopen('https://pypi.python.org/pypi/amiyabot/json').read())
-v_list = {ver_num(v): v for v in pypi['releases'].keys()}
-s_list = sorted(v_list)
-latest = v_list[s_list[-1]]
+    print(f'latest: {latest}')
 
-print(f'latest: {latest}')
+    return f"{latest}"
+    # return f"1.10.10"
 
-latest = latest.split('.')
-latest[-1] = str(int(latest[-1]) + 1)
+# Auto increment the version number.
+# 1.0.9 -> 1.1.0 , 1.9.9 -> 2.0.0 but 9.9.9 -> 10.0.0
+def incr_version(v):
+    v = v.split('.')
+    if len(v) == 3:
+        if int(v[2]) >= 9:
+            v[2] = '0'
+            if int(v[1]) >= 9:
+                v[1] = '0'
+                v[0] = str(int(v[0]) + 1)
+            else:
+                v[1] = str(int(v[1]) + 1)
+        else:
+            v[2] = str(int(v[2]) + 1)
+    else:
+        v.append('1')
+    return '.'.join(v)
 
-new_version = '.'.join(latest)
-release_new = input(f'new?: {new_version} (Y/n)')
+class CustomBdistWheelCommand(_bdist_wheel):
+    user_options = _bdist_wheel.user_options + [
+        ('auto-increment-version', None, 'Auto increment the version number before building with special rule: 1.0.9 -> 1.1.0 , 1.9.9 -> 2.0.0 . However 9.9.9 -> 10.0.0')
+    ]
 
-if not (not release_new or release_new.lower() == 'y'):
-    new_version = ''
+    def initialize_options(self):
+        _bdist_wheel.initialize_options(self)
+        self.auto_increment_version = False
+
+    def finalize_options(self):
+        _bdist_wheel.finalize_options(self)
+        latest_version = get_new_version()
+        if self.auto_increment_version:
+            new_version = incr_version(latest_version)
+            print(f'Auto-incrementing version to: {new_version}')
+            self.distribution.metadata.version = new_version
+        else:
+            new_version = incr_version(latest_version)
+            release_new = input(f'new?: {new_version} (Y/n)')
+
+            if not (not release_new or release_new.lower() == 'y'):
+                new_version = input('version: ')
+
+        self.distribution.metadata.version = new_version
+
+    def run(self):
+        _bdist_wheel.run(self)
 
 with open('README.md', mode='r', encoding='utf-8') as md:
     description = md.read()
@@ -44,7 +84,7 @@ for root, dirs, files in os.walk('amiyabot/_assets'):
 
 setuptools.setup(
     name='amiyabot',
-    version=new_version or input('version: '),
+    version="0.0.1",
     author='vivien8261',
     author_email='826197021@qq.com',
     url='https://www.amiyabot.com',
@@ -57,6 +97,9 @@ setuptools.setup(
     include_package_data=True,
     python_requires='>=3.8',
     install_requires=requirements,
+    cmdclass={
+        'bdist_wheel': CustomBdistWheelCommand,
+    },
 )
 
 # python setup.py bdist_wheel
