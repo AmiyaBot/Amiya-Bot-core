f985d82e86e67858848f20873c63d3d05c1b65a1
Vivien <34387011+vivien8261@users.noreply.github.com>
2024-01-25 15:24:38 +0800

    update: 全局的 logging.Handler

--- amiyabot/log/manager.py
+++ amiyabot/log/manager.py
@@ -7,14 +7,16 @@ import traceback
 from typing import Union, Dict, List, Tuple, Type, Any, Callable, Optional, Awaitable
 from contextlib import asynccontextmanager, contextmanager
 from concurrent_log_handler import ConcurrentRotatingFileHandler
-from amiyabot.util import argv
+from amiyabot.util import argv, create_dir
 
 FETCHER = Callable[[str, str, Tuple[Any, ...], Dict[str, Any]], None]
 
 
 class LoggerManager:
     user_logger = None
-    log_fetchers: Dict[int, FETCHER] = {}
+
+    log_fetchers: List[FETCHER] = []
+    log_handlers: List[logging.Handler] = []
 
     def __init__(
         self,
@@ -24,8 +26,6 @@ class LoggerManager:
         save_path: str = './log',
         save_filename: str = 'running',
     ):
-        self.handlers: Dict[str, logging.Logger] = {}
-
         self.name = name
         self.debug_mode = argv('debug', bool)
 
@@ -41,54 +41,73 @@ class LoggerManager:
         self.save_path = save_path
         self.save_filename = save_filename
 
+        formatter = logging.Formatter(self.formatter)
+
+        self.file_handler = ConcurrentRotatingFileHandler(
+            filename=f'{self.save_path}/{self.save_filename}.log',
+            encoding='utf-8',
+            maxBytes=512 * 1024,
+            backupCount=10,
+        )
+        self.file_handler.setFormatter(formatter)
+        self.file_handler.setLevel(self.level)
+
+        self.stream_handler = logging.StreamHandler(stream=sys.stdout)
+        self.stream_handler.setFormatter(formatter)
+        self.stream_handler.setLevel(self.level)
+
+        self.loggers: Dict[str, logging.Logger] = {}
+
     @classmethod
     def use(cls, logger_cls):
         cls.user_logger = logger_cls()
 
     @classmethod
     def add_fetcher(cls, func: FETCHER):
-        cls.log_fetchers[id(func)] = func
+        cls.log_fetchers.append(func)
         return func
 
     @classmethod
     def remove_fetcher(cls, func: FETCHER):
-        if id(func) in cls.log_fetchers:
-            del cls.log_fetchers[id(func)]
+        if func in cls.log_fetchers:
+            cls.log_fetchers.remove(func)
+
+    @classmethod
+    def add_handler(cls, handler: logging.Handler):
+        cls.log_handlers.append(handler)
+        return handler
+
+    @classmethod
+    def remove_handler(cls, handler: logging.Handler):
+        if handler in cls.log_handlers:
+            cls.log_handlers.remove(handler)
 
     @property
-    def __handler(self):
+    def __logger(self):
         if self.user_logger and self != self.user_logger:
             return self.user_logger
 
-        if self.save_filename not in self.handlers:
-            if not os.path.exists(self.save_path):
-                os.makedirs(self.save_path)
-
-            formatter = logging.Formatter(self.formatter)
-
-            file_handler = ConcurrentRotatingFileHandler(
-                filename=f'{self.save_path}/{self.save_filename}.log',
-                encoding='utf-8',
-                maxBytes=512 * 1024,
-                backupCount=10,
-            )
-            file_handler.setFormatter(formatter)
-            file_handler.setLevel(self.level)
-
-            stream_handler = logging.StreamHandler(stream=sys.stdout)
-            stream_handler.setFormatter(formatter)
-            stream_handler.setLevel(self.level)
-
+        if self.save_filename not in self.loggers:
             logger = logging.getLogger(name=self.name)
             logger.setLevel(self.level)
 
-            if not logger.handlers:
-                logger.addHandler(file_handler)
-                logger.addHandler(stream_handler)
+            if not logger.hasHandlers():
+                logger.addHandler(self.file_handler)
+                logger.addHandler(self.stream_handler)
 
-            self.handlers[self.save_filename] = logger
+            self.loggers[self.save_filename] = logger
 
-        return self.handlers[self.save_filename]
+        create_dir(self.save_path)
+        _logger = self.loggers[self.save_filename]
+
+        for h in set(self.log_handlers) - set(_logger.handlers):
+            _logger.addHandler(h)
+
+        for h in set(_logger.handlers) - set(self.log_handlers):
+            if h != self.file_handler and h != self.stream_handler:
+                _logger.removeHandler(h)
+
+        return _logger
 
     @property
     def print_method(self):
@@ -107,7 +126,7 @@ class LoggerManager:
 
             level = inspect.getframeinfo(inspect.currentframe().f_back)[2]
 
-            for func in self.log_fetchers.values():
+            for func in self.log_fetchers:
                 func(text, level, *args, **kwarg)
 
             return output
@@ -115,17 +134,17 @@ class LoggerManager:
         return method
 
     def info(self, text: str, *args, **kwarg):
-        self.__handler.info(
+        self.__logger.info(
             self.print_method(text, *args, **kwarg),
         )
 
     def debug(self, text: str, *args, **kwarg):
-        self.__handler.debug(
+        self.__logger.debug(
             self.print_method(text, *args, **kwarg),
         )
 
     def warning(self, text: str, *args, **kwarg):
-        self.__handler.warning(
+        self.__logger.warning(
             self.print_method(text, *args, **kwarg),
         )
 
@@ -135,7 +154,7 @@ class LoggerManager:
         if desc:
             text = f'{desc} {text}'
 
-        self.__handler.error(
+        self.__logger.error(
             self.print_method(text, desc=desc, *args, **kwarg),
         )
 
@@ -144,7 +163,7 @@ class LoggerManager:
     def critical(self, err: Union[str, Exception], *args, **kwarg):
         text = traceback.format_exc() if isinstance(err, Exception) else str(err)
 
-        self.__handler.critical(
+        self.__logger.critical(
             self.print_method(text, *args, **kwarg),
         )
 
